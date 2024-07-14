1d16587fba0cdee2ba63217746632c6bb09fb05e
Erik de Vries <erikdv@online.nl>
2024-07-14 11:58:28 +0200

    Removed CORS annotations

--- src/main/kotlin/net/eriknet/sf2/message/controller/MessageController.kt
+++ src/main/kotlin/net/eriknet/sf2/message/controller/MessageController.kt
@@ -1,9 +1,7 @@
 package net.eriknet.sf2.message.controller
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import net.eriknet.sf2.message.model.Message
 import net.eriknet.sf2.message.service.MessageService
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,13 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 class MessageController(
     private val messageService: MessageService
 ) {
-    @CrossOrigin("http://localhost:3000")
     @GetMapping("/api/messages")
     fun listAll(): List<MessageResponse> =
         messageService.findAll()
             .map { it.toResponse() }
 
-    @CrossOrigin("http://localhost:3000")
     @PostMapping("/api/message")
     fun post(@RequestBody request: NewMessageRequest)=
         messageService.save(request.toMessage())
