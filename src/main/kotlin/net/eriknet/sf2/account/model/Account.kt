7521fdc6bcfb35fc47431cff46a0d421a88853a9
Erik de Vries <erikdv@online.nl>
2025-01-12 20:40:57 +0100

    Adding roles

--- src/main/kotlin/net/eriknet/sf2/account/model/Account.kt
+++ src/main/kotlin/net/eriknet/sf2/account/model/Account.kt
@@ -1,17 +1,25 @@
 package net.eriknet.sf2.account.model
 
-import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
 import java.util.*
 
 @Entity
 class Account(
     val username: String,
     val password: String,
-    @ElementCollection(targetClass = Role::class)
-    val roles: List<Role> = listOf(Role.USER)
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "account_roles",
+        joinColumns = [JoinColumn(name = "account_id")],
+        inverseJoinColumns = [JoinColumn(name = "role")]
+    )
+    val roles: Set<Role> = setOf()
 ) {
     @Id
     @GeneratedValue(generator = "UUID")
